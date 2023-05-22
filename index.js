2532fa43b1aba31664704ab583f5a67556c5ed92
PritrirajPartho <dapartho5616@gmail.com>
2023-05-23 04:08:10 +0600

    lasta-added

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
-// Middleware is here
+// Middleware is here........
 app.use(cors())
 app.use(express.json());
 // -------------------------------------------------------------
@@ -101,9 +101,9 @@ run().catch(console.log);
 
 // -------------------------------------------------------------
 app.get('/', (req, res) => {
-  res.send('assignment 11 is running ...........')
+  res.send('Assignment 11 is running ...........')
 })
 
 app.listen(port, () => {
-  console.log(`Assignment app listening on port ${port}`)
+  console.log(`Assignment node app listening on port ${port}`)
 })
